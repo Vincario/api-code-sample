b20702e70897a160aabadfa30dd53427998183d1
Adrian Mindek <adrian.mindek@gamil.com>
2023-02-08 11:54:42 +0100

    Added comments and refactor

--- Widgets/VIN_WIDGET/js/app.js
+++ Widgets/VIN_WIDGET/js/app.js
@@ -1,14 +1,21 @@
+/**
+ * Redirect to URL with decode results
+ */
 function decodeAction() {
     const errors = validateInput();
     const vinInput = document.getElementById('vinDecodeInput');
 
-    if(errors.length === 0){
-        window.location.href = "https://vindecoder.eu/cz/check-vin/" + vinInput.value;
-    }else{
+    if (errors.length === 0) {
+        window.open("https://vindecoder.eu/cz/check-vin/" + vinInput.value, '_blank');
+    } else {
         showErrors(true, errors);
     }
 }
 
+/**
+ * Validate input function
+ * @returns {any[]}
+ */
 function validateInput() {
     const vinInput = document.getElementById('vinDecodeInput');
     const errors = Array();
@@ -16,41 +23,52 @@ function validateInput() {
 
     vinInput.value = vinInput.value.trim();
 
-    if(vinInput.value === ""){
+    if (vinInput.value === "") {
         errors.push('Input can´t be empty...');
-    }
-    else if(!regExp.test(vinInput.value)){
-        errors.push('Only letters and numbers allowed excluded I,Q and O letters.');
+    } else if (!regExp.test(vinInput.value)) {
+        errors.push('Only letters and numbers allowed. Excluded I,Q and O letters.');
     }
 
     return errors;
 }
 
-function showErrors(show, errors){
+/**
+ * Show/hide error messages
+ * @param show : Boolean
+ * @param errors : Array
+ */
+function showErrors(show, errors) {
     const errorsMessages = document.querySelector('#vincario-decode-vin-widget .errors');
 
-    if(errors.length !== 0){
+    if (errors.length !== 0) {
         errorsMessages.innerHTML = "";
 
-        for (let i =0; i < errors.length; i++){
+        for (let i = 0; i < errors.length; i++) {
             const el = document.createElement('p');
             el.innerText = errors[i];
             errorsMessages.append(el);
         }
     }
 
-    if(show){
+    if (show) {
         errorsMessages.classList.remove('display-none');
         return;
     }
     errorsMessages.classList.add('display-none');
 }
 
-function changeInput(){
+/**
+ * Change input event
+ */
+function changeInput() {
     const errors = validateInput();
     showErrors(errors.length !== 0, errors);
-
 }
+
+/**
+ * On load document event action
+ * @param event
+ */
 window.onload = (event) => {
     const vinInput = document.getElementById('vinDecodeInput');
 
